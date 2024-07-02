0c38a951a511afb85f060e5ca1ecc41156064ae5
Miguel Arandia <143758416+marandia513@users.noreply.github.com>
2024-07-02 12:39:09 -0300

    Update index.js
    modificaciones para integrar al front

--- index.js
+++ index.js
@@ -1,32 +1,33 @@
-// index.js
 const express = require("express");
-const app = express();
-const authRoutes = require("./routes/routes.auth");
+const dotenv = require("dotenv");
 const helmet = require("helmet");
 const rateLimit = require("express-rate-limit");
+const authRoutes = require("./routes/routes.auth");
+const app = express();
+
+// Configurar dotenv
+dotenv.config();
 
 // Middleware para seguridad
 app.use(helmet());
-
-// Limitador de tasa
 const limiter = rateLimit({
-    windowMs: 15 * 60 * 1000, // 15 minutes
-    max: 100 // limit each IP to 100 requests per windowMs
+  windowMs: 15 * 60 * 1000, // 15 minutes
+  max: 100, // limit each IP to 100 requests per windowMs
 });
 app.use(limiter);
 
 // Middleware para parsear JSON
 app.use(express.json());
 
-// Rutas para autenticación
-app.use('/auth', authRoutes);
+// Rutas para autenticación y formularios
+app.use("/auth", authRoutes);
 
 // Servir archivos estáticos desde la carpeta 'public'
 app.use(express.static("public"));
 
 // Ruta principal
 app.get("/", (req, res) => {
-    res.send("¡Bienvenidos al proyecto Backend del grupo 18!");
+  res.send("¡Bienvenidos al proyecto Backend del grupo 18!");
 });
 
 // Puerto de escucha
@@ -34,4 +35,3 @@ const PORT = process.env.PORT || 3000;
 
 // Iniciar el servidor
 app.listen(PORT, () => console.log(`Servidor corriendo en http://localhost:${PORT}`));
-
